0c3c14d7f6a622e3ae80d63a20fe0727a3744845
Rasmus Lagerström <rasla833@student.liu.se>
2021-05-17 16:35:47 +0200

    Implementerat Rocket_Projectile

--- include/Bullet.h
+++ include/Bullet.h
@@ -2,12 +2,13 @@
 #define BULLET_H
 
 #include <SFML/Graphics.hpp>
+#include "Resource_Manager.h"
+
 
 class Bullet
 {
     public:
         Bullet(sf::Vector2f pos, float rot);    //Koordinater för spelaren, rotationen för spelaren (just nu i radianer)
-
         void update();
         void render(sf::RenderTarget&);
 
@@ -16,9 +17,30 @@ class Bullet
         void reverse_x();                       //Byt riktning i x-led
         void reverse_y();                       //Byt riktning i y-led
         int lifetime;                           //Antal studs kulan har kvar att leva
-    private:                       
+    protected:                       
         sf::Vector2f velocity;                  //Hastighet
+        
+    private:
         sf::CircleShape sprite;
+};
+
+class Rocket_Projectile
+{
+    public:
+        Rocket_Projectile(sf::Vector2f pos, float rot);    //Koordinater för spelaren, rotationen för spelaren (just nu i radianer)
+        void update();
+        void render(sf::RenderTarget&);
+
+        sf::FloatRect getBounds();              //Returnerar GlobalBounds för sprite
+        sf::Vector2f get_velocity();            //Returnera hastigheten (pixlar per frame)
+        void reverse_x();                       //Byt riktning i x-led
+        void reverse_y();                       //Byt riktning i y-led
+        int lifetime;                           //Antal studs kulan har kvar att leva
+    protected:                       
+        sf::Vector2f velocity;                  //Hastighet
+        
+    private:
+        sf::Sprite sprite;
 };
 
 #endif
